a6662d5c78598226b355f9ba9f7d986bba132da5
JackyShen <mebusw@163.com>
2015-07-14 19:22:49 +0800

    move static methods to base class Mapper. use Capitalized name for DSL methods.

--- src/mapper.py
+++ src/mapper.py
@@ -3,7 +3,7 @@ __author__ = 'mebusw@163.com'
 
 class Mapper(object):
     @staticmethod
-    def ATTR(by):
+    def Attr(by):
         def f(instance, domain_label):
             print 'ATTR: %s ==> %s' % (by, domain_label)
             return getattr(instance, by)
@@ -11,21 +11,31 @@ class Mapper(object):
         return f
 
     @staticmethod
-    def WRAP(clzVO, keys=[]):
+    def Wrap(clz_vo, keys=[]):
         def f(instance, domain_label):
-            print 'WRAP: %s ==> %s ^ %s ' % (keys, domain_label, clzVO)
-            vo = clzVO()
+            print 'WRAP: %s ==> %s ^ %s ' % (keys, domain_label, clz_vo)
+            vo = clz_vo()
             for key in keys:
                 setattr(vo, key, getattr(instance, key))
             return vo
 
         return f
 
-#
-# if __name__ == '__main__':
-#     # Auto Discover
-#     UserMapper.prepare()
-#
-#     user_model = UserModel()
-#     user = user_model.to_vo()
-#     print user
+    @classmethod
+    def prepare(clazz):
+        _meta = vars(clazz)
+        _fields = dict(
+            (key, value) for (key, value) in _meta.iteritems() if not callable(value) and not key.startswith('__'))
+        _mappings = dict((key, value) for (key, value) in _meta.iteritems() if
+                         not key.startswith('__') and not key in ['domain', 'prepare', 'model'])
+        # print _mappings
+        clazz.clzDomain = _meta['domain']
+        clazz.clzModel = _meta['model']
+
+        def to_vo(instance):
+            domain_obj = clazz.clzDomain()
+            for (label, func) in _mappings.iteritems():
+                setattr(domain_obj, label, func(instance, label))
+            return domain_obj
+
+        setattr(clazz.clzModel, 'to_vo', to_vo)
